928a2f6bd9889df85b2897a65023707c67f2d108
Qlorry <kostik.cu4er@gmail.com>
2024-02-05 22:21:40 +0200

    simple login with some docs (#8)

--- Sources/Frontend/src/router/index.ts
+++ Sources/Frontend/src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import CallbackPage from "../views/CallbackPage.vue";
+
+import { authGuard } from "@auth0/auth0-vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,8 +18,14 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      component: () => import('../views/AboutView.vue'),
+      beforeEnter: authGuard,
+    },
+    {
+      path: "/callback",
+      name: "callback",
+      component: CallbackPage,
+    },
   ]
 })
 
--- Sources/Frontend/vite.config.ts
+++ Sources/Frontend/vite.config.ts
@@ -12,5 +12,19 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    host: true,
+    strictPort: true,
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5168/',
+        changeOrigin: true,
+        configure: (proxy, options) => {
+          // proxy will be an instance of 'http-proxy'
+        }
+      }
+    }
   }
 })
